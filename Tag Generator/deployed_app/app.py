fe77575ed0a55d11b7043325e41d3ba6e19711e8
Godwin <ekainugodwin@gmail.com>
2020-01-22 13:25:17 +0100

    fixed webpages and content issues

---
+++ Tag Generator/deployed_app/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template
+import os
+from text_modelling import vectorize_words, nmf, show_words
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/modelling', methods=['GET', 'POST'])
+def model():
+    if request.method == 'POST':
+        text = request.form['text']
+        return render_template('model.html', result=request.form['text']), text
+    if request.method == 'GET':
+        return render_template('model.html')
+
+
+@app.route('/text', methods=['POST'])
+def texts():
+    text = request.form['text']
+    text = [text]
+    length = request.form['length']
+    length = int(length)
+    vocabs, vectors = vectorize_words(text=text)
+    nmf_comp = nmf(vectors)
+    words = show_words(nmf_comp, length, vocabs)
+    return render_template('text.html', words=words)
+
+
+if __name__ == '__main__':
+    app.run()
